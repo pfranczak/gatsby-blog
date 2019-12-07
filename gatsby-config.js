f5a986b4df73ad40ca847fee9b029f292ebfd1a2
pfranczak <franczak.przemyslaw3@gmail.com>
2019-12-07 21:41:25 +0100

    Gatsby config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,15 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: 'Blog',
+    description: 'IT blog'
+  },
+  plugins: [
+    `gatsby-transformer-remark`, {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`
+      }
+    }
+  ]
 }
