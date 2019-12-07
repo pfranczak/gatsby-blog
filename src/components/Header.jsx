2689aca4587349bf2acc8db4d8542df8ceb1cdb0
pfranczak <franczak.przemyslaw3@gmail.com>
2019-12-07 22:28:42 +0100

    Display post list

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { StaticQuery, graphql } from 'gatsby';
 const TitleAndDescription = ({data: {title, description}}) => (
     <div>
         <h1>{title}</h1>
-        <h3>{description}</h3>
+        <h2>{description}</h2>
     </div>
 );
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
 import Header from '../components/Header';
 
-const Layout = () => {
+const Layout = ({data: {allMarkdownRemark: {edges}}}) => {
     return (
         <div>
             <Header/>
+            {edges.map(({node: {frontmatter: {path, title, date}}}) => 
+                <div key={path}>
+                    <h3>{title}</h3>
+                    <p>{date}</p>
+                </div>
+            )}
         </div>
     );
 }
+
+export const postsQuery = graphql`
+    query HomepageQuery {
+         allMarkdownRemark {
+            edges {
+                node {
+                    frontmatter {
+                        title
+                        path
+                        date
+                    }
+                }
+            }
+        }
+    }
+`;
 
 export default Layout;
